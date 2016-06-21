a7d44313ae6e3e0b60199d78a712f293355a8639
Noevain <noevain@gmail.com>
2016-06-21 03:56:24 +0200

    only organisation since @LostJourneyer  didn't commited his changes yet,I took the liberty to do the organisation,merged hp and mov,removed stard assets,created placeholder folder(Use placeholder for everything that will need testing for later on)next commit should make death being a thing and add an ennemy and a way to damage the ennemy

---
+++ Notatest/Assets/Script/Loadlevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loadlevel : MonoBehaviour {
+
+    public GameObject Loadingscreen;
+    public void LoadScene(int scene)
+    {
+        Loadingscreen.SetActive(true);
+        LoadScene(scene);
+    }
+}
---
+++ Notatest/Assets/Script/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mov : MonoBehaviour {
+    public float speed = 4f;
+    public GameObject stuffthatismoving;
+    public float health = 100f;
+
+    private Loadlevel Loadlevel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            stuffthatismoving.transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            stuffthatismoving.transform.Translate(-Vector2.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            stuffthatismoving.transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            stuffthatismoving.transform.Translate(-Vector3.back * speed * Time.deltaTime);
+        }
+        if (health <= 0f);
+        {
+            //for now only restart the scene,when we will have death animation
+            //just lock the axis and call animation here
+
+        }
+
+    }
+}
